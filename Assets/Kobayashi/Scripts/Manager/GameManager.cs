46141f67facc7a45c76f4f8aaef7fc28a8632a0b
KobayashiTetsuya <qtetsu.8201.tetsuq@gmail.com>
2025-10-22 02:58:18 +0900

    [add]行動選択ボタンの基盤作成

--- Assets/Kobayashi/Scripts/Manager/GameManager.cs
+++ Assets/Kobayashi/Scripts/Manager/GameManager.cs
@@ -7,7 +7,8 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance;
     private Player _player;
     private Enemy _enemy;
-    private InGamePhase _gamePhase;
+    private UIManager _uiManager;
+    public InGamePhase _gamePhase;
     private SceneDivision _currentScene;
     private void Awake()
     {
@@ -20,14 +21,38 @@ public class GameManager : MonoBehaviour
         Instance = this;
         DontDestroyOnLoad(gameObject);
 
+        //後でswitchに入れてエラー対策行う
         _player = FindAnyObjectByType<Player>();
         _enemy = FindAnyObjectByType<Enemy>();
+        _uiManager = GetComponent<UIManager>();
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        
+        switch (_currentScene)
+        {
+            case SceneDivision.Title://タイトルシーンで実行したいこと
+
+            break;
+            case SceneDivision.InGame://インゲームシーンで実行したいこと
+                switch (_gamePhase)
+                {
+                    case InGamePhase.CountDown:
+
+                    break;
+                    case InGamePhase.Chose:
+
+                    break;
+                    case InGamePhase.Attack:
+
+                    break;
+                }
+            break;
+            case SceneDivision.Result://リザルトシーンで実行したいこと 
+
+            break;
+        }
     }
 }
--- Assets/Kobayashi/Scripts/Manager/UIManager.cs
+++ Assets/Kobayashi/Scripts/Manager/UIManager.cs
@@ -9,17 +9,21 @@ public class UIManager : MonoBehaviour
     [Header("キャラクター")]
     [Tooltip("プレイヤー"),SerializeField] private Image _playerImage;
     [Tooltip("エネミー"),SerializeField] private Image _enemyImage;
+    [Header("パネル")]
+    [Tooltip("行動選択"), SerializeField] private GameObject _activeChosePanel;
+    [Header("ボタン")]
+    [Tooltip("攻撃"), SerializeField] private Button _attackButton;
+    [Tooltip("防御"),SerializeField] private Button _defenseButton;
+    [Tooltip("溜める"), SerializeField] private Button _accumulateButton;
+    private bool _chose = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        ResetState();
+        _attackButton.onClick.AddListener(Attack);
+        _defenseButton.onClick.AddListener(Defense);
+        _accumulateButton.onClick.AddListener(Accumulate);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     /// <summary>
     /// UIの値をリセット
     /// </summary>
@@ -27,5 +31,17 @@ public class UIManager : MonoBehaviour
     {
         _playerHP.value = 1;
         _enemeyHP.value = 1;
+    }
+    private void Attack()
+    {
+        Debug.Log("攻撃");
+    }
+    private void Defense()
+    {
+        Debug.Log("防御");
+    }
+    private void Accumulate()
+    {
+        Debug.Log("溜める");
     }
 }
