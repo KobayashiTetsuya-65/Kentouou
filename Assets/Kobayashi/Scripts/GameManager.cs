873cf8f2b30a712fb3f49441f92c3d65e9206aac
KobayashiTetsuya <qtetsu.8201.tetsuq@gmail.com>
2025-10-21 15:56:28 +0900

    [add]GameManager基盤

--- Assets/Kobayashi/Scripts/GameManager.cs
+++ Assets/Kobayashi/Scripts/GameManager.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager Instance;
+    private void Awake()
     {
-        
+        Application.targetFrameRate = 100;
+        if(Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
     }
+
 
     // Update is called once per frame
     void Update()
