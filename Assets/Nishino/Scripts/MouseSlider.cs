4a09ca1911bfed27ddb71fdb2b1497238c581d43
KobayashiTetsuya <qtetsu.8201.tetsuq@gmail.com>
2025-11-10 15:24:05 +0900

    [remove]カーソルの感度調整

--- Assets/Nishino/Scripts/MouseSlider.cs
+++ Assets/Nishino/Scripts/MouseSlider.cs
@@ -3,25 +3,25 @@ using UnityEngine.UI;
 
 public class MouseSlider : MonoBehaviour
 {
-    [Header("マウス感度スライダー")]
-    public Slider sensitivitySlider;
+    //[Header("マウス感度スライダー")]
+    //public Slider sensitivitySlider;
 
-    private float savedValue;
+    //private float savedValue;
 
-    private void Start()
-    {
-        savedValue = PlayerPrefs.GetFloat("MouseSensitivity", 1f);
-        sensitivitySlider.SetValueWithoutNotify(savedValue);
-        if (GlobalCursor.Instance != null)
-        {
-            GlobalCursor.Instance.SetSensitivity(savedValue);
-        }
-        sensitivitySlider.onValueChanged.AddListener((value) =>
-        {
-            if(GlobalCursor.Instance != null)
-                GlobalCursor.Instance.SetSensitivity(value);
-            PlayerPrefs.SetFloat("MouseSensitivity", value);
-            PlayerPrefs.Save();
-        });
-    }
+    //private void Start()
+    //{
+    //    savedValue = PlayerPrefs.GetFloat("MouseSensitivity", 1f);
+    //    sensitivitySlider.SetValueWithoutNotify(savedValue);
+    //    if (GlobalCursor.Instance != null)
+    //    {
+    //        GlobalCursor.Instance.SetSensitivity(savedValue);
+    //    }
+    //    sensitivitySlider.onValueChanged.AddListener((value) =>
+    //    {
+    //        if(GlobalCursor.Instance != null)
+    //            GlobalCursor.Instance.SetSensitivity(value);
+    //        PlayerPrefs.SetFloat("MouseSensitivity", value);
+    //        PlayerPrefs.Save();
+    //    });
+    //}
 }
